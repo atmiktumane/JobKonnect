8b9d9208d38957e6d0c5c433f7a28dbbc9d3f1b8
atmik-oneranks <atmiktumane72@gmail.com>
2025-06-16 09:00:08 +0530

    RangeSlider {Mantine Component} + Advance Tailwind CSS

--- src/Components/FindJobs/SearchBar.tsx
+++ src/Components/FindJobs/SearchBar.tsx
@@ -1,8 +1,12 @@
-import { Divider } from "@mantine/core";
+import { Divider, RangeSlider } from "@mantine/core";
 import { dropdownData } from "../../Data/JobsData";
 import { MultiInput } from "./MultiInput";
+import { useState } from "react";
 
 export const SearchBar = () => {
+  // State : for Salary Range Slider
+  const [value, setValue] = useState<[number, number]>([3, 90]);
+
   return (
     <div className="flex gap-3 px-5 py-10">
       {dropdownData.map((item, index) => (
@@ -13,6 +17,25 @@ export const SearchBar = () => {
           <Divider size="xs" orientation="vertical" />
         </>
       ))}
+
+      {/* Salary Range Selector */}
+      <div className="w-1/5 text-sm [&_.mantine-Slider-label]:!translate-y-10">
+        {/* Row 1 - Heading */}
+        <div className="flex items-center justify-between">
+          <h5>Salary</h5>
+          <p>
+            &#8377;{value[0]} LPA - &#8377;{value[1]} LPA
+          </p>
+        </div>
+
+        {/* Row 2 - Range Selector */}
+        <RangeSlider
+          color="brightSun.4"
+          size="xs"
+          value={value}
+          onChange={setValue}
+        />
+      </div>
     </div>
   );
 };
